43b86307c6e30f36eeffa3032f832bba66ec9e7b
dejwona <iwograf@gmail.com>
2017-06-25 19:49:06 +0200

    animation

--- js/app.js
+++ js/app.js
@@ -1,31 +1,34 @@
 $(function() {
   var $header = $('header');
   var $mainNav = $header.find('.main-nav');
-  var $mainNavItem = $mainNav.find('.main-nav-item');
-  var $mainNavIconContent = $mainNavItem.find('.main-nav-icon-content');
-  var $mainNavIconBghex = $mainNavItem.find('.main-nav-icon-bghex');
+  var $mainNavItem = $mainNav.find('.main-nav-item a');
 
 
-  $mainNavIconContent.on('mouseover', function() {
+  $mainNavItem.on('mouseover', function() {
     $(this).next().children('div').css( "background-color", "#492445" );
     $(this).next().children('div').animate({
       opacity: '1',
     }, 100);
   });
-   $mainNavIconContent.on('mouseleave', function() {
+   $mainNavItem.on('mouseleave', function() {
     $(this).next().children('div').animate({
       opacity: '0.35',
     }, 100);
     $(this).next().children('div').css( "background-color", "white" );
   });
-   $mainNavIconContent.on('click', function() {
+   $mainNavItem.on('click', function() {
      $(this).next().children('div').css( "background-color", "#492445" );
      $(this).next().children('div').animate({
        opacity: '1',
   }, 100);
   });
 
-  $("#example-one").on("click", function() {
+
+  $('.carousel').carousel({
+  interval: 500
+})
+
+  $("#example-two").on("click", function() {
     var el = $(this);
     if (el.text() == el.data("text-swap")) {
       el.text(el.data("text-original"));
@@ -50,30 +53,4 @@ $(function() {
 
 });
 
-  var $window           = $(window),
-      win_height_padded = $window.height() * 1.1,
-      isTouch           = Modernizr.touch;
- $window.on('scroll', revealOnScroll);
-
- function revealOnScroll() {
-  var scrolled = $window.scrollTop();
-
-  $(".revealOnScroll:not(.web-design)").each(function () {
-    var $this     = $(this),
-        offsetTop = $this.offset().top;
-
-    if (scrolled + win_height_padded > offsetTop) {
-      if ($this.data('timeout')) {
-        window.setTimeout(function(){
-          $this.addClass('web-design ' + $this.data('animation'));
-        }, parseInt($this.data('timeout'),10));
-      } else {
-        $this.addClass('web-design ' + $this.data('animation'));
-      }
-    }
-  });
-
-
-}
-
-})
+});
